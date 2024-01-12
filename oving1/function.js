bfdf10da68c44fdee2a07902278364a1e281fe78
anderebe <anderebe@stud.ntnu.no>
2024-01-12 13:21:17 +0100

    work to oving1.4

--- oving1/function.js
+++ oving1/function.js
@@ -1,17 +1,41 @@
-var audio = new Audio('https://ia601700.us.archive.org/5/items/polish-cow-full-version/Polish%20Cow%20Full%20Version.mp3'); // Replace 'path/to/audio.mp3' with the actual path to your audio file
 
 function revealCow() {
-    var gifElement = document.querySelector('.gif');
+    var gifVideo = document.querySelector('.gif img');
+    var gifAudio = document.querySelector('.gif audio');
 
-    if (audio.paused) {
-        gifElement.style.opacity = '1';
-        audio.play();
-    } else if (audio.played) {
-        gifElement.style.opacity = '0';
-        audio.pause();
+    if (gifAudio.paused) {
+        gifVideo.style.opacity = '1';
+        gifAudio.play();
+    } else if (gifAudio.played) {
+        gifVideo.style.opacity = '0';
+        gifAudio.pause();
     }
 }
 
+function changeGif() {
+    var gifVideo = document.querySelector('.gif img');
+    var gifAudio = document.querySelector('.gif audio');
+    var button = document.querySelector('.gif-button');
+
+    gifVideo.style.opacity = '0';
+    gifAudio.pause();
+
+    if (gifVideo.src.includes('ahmed-musin.gif')) {
+        setTimeout(function() {
+            gifVideo.src = 'gifs/polish-cow-cow.gif';
+            gifAudio.src = 'audio/polish-cow-cow.mp3';
+            button.style.color = 'white';
+            button.style.textShadow = 'none';
+        }, 200); // Add a delay of 200 milliseconds
+    } else {
+        setTimeout(function() {
+            gifVideo.src = 'gifs/ahmed-musin.gif';
+            gifAudio.src = 'audio/ahmed-musin.mp3';
+            button.style.color = 'blue';
+            button.style.textShadow = '0 0 5px white';
+        }, 200); // Add a delay of 200 milliseconds
+    }
+}
 function newPage1() {
     window.location.href = "https://www.youtube.com/watch?v=2ZIpFytCSVc";
 }
