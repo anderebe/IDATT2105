6409f34cb2d4bfe8ef5a7f21606fa12bf0ea714e
anderebe <anderebe@stud.ntnu.no>
2024-02-02 16:44:27 +0100

    Oving3.03: small work

--- oving3/src/App.vue
+++ oving3/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav-header />
+  <div class = 'content'>
+    <h1>Velkommen til min side!</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavHeader from './components/NavHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavHeader,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    grid-row: auto;
+  }
+
+  body {
+    background-image: url("@/assets/images/landscapecss2.jpg");
+    background-color: #ffff;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  content{
+  }
+
+  
 </style>
---
+++ oving3/src/components/NavHeader.vue
@@ -0,0 +1,78 @@
+<template>
+    <nav>
+        <img src="../assets/images/highfiveslogo.png" class="logo" @click="newPage1">
+        <ul>
+            <li><a href='#' @click="goHome">HOME</a></li>
+            <li><a href='#' @click="goSettings">SETTINGS</a></li>
+            <li><a href='#' @click="toCalculator">CALCULATOR</a></li>
+            <li><a class='gif-button' @click="changeGif">GIF</a></li>
+            <li><a href='#' @click="goClose">LOGOUT</a></li>
+        </ul>
+    </nav>
+  </template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  nav{
+      width: 95%;
+      height: 10vh;
+      position: absolute;
+      top: 0;
+      padding-top: 20px, 8%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+  }
+
+  nav .logo{
+      width: 80px;
+      margin-top: 5px;
+      margin-left: 5px;
+      cursor: pointer;
+  }
+
+  nav .logo:active {
+      transform: translateY(1px);
+  }
+
+  nav ul li{
+      list-style: none;
+      display: inline-block;
+      margin-left: 20px;
+  }
+
+  nav ul li a{
+      text-decoration: none;
+      color: white;
+      font-size: 17px;
+      position: relative;
+      cursor: pointer;
+  }
+
+  .gif-button{
+      color: white;
+  }
+
+  nav ul li a::after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      bottom: -2px;
+      left: 0;
+      background-color: white;
+      transform: scaleX(0);
+      transition: transform 0.3s ease-in-out;
+  }
+
+  nav ul li a:hover::after {
+      transform: scaleX(1);
+  }
+</style>
