45a7f25dbf8540fc7d814006f1f1be32fef47aba
anderebe <anderebe@stud.ntnu.no>
2024-02-07 19:16:26 +0100

    Oving3.11.1: Fixed bugs

--- oving3/src/App.vue
+++ oving3/src/App.vue
@@ -17,4 +17,27 @@
   color: #2c3e50;
   grid-row: auto;
 }
+
+body {
+  background-image: url("@/assets/images/landscapecss2.jpg");
+  background-color: #ffff;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
